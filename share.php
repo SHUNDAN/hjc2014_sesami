1a112a9773d642802a4aef6296c594fa24329ee2
Yohei Munesada <y.munesada@gmail.com>
2014-05-10 00:29:25 +0900

    add empty html

---
+++ share.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="jp">
+<head>
+	<meta charset="UTF-8">
+	<title>オリジナル写真を作ろう</title>
+	<link rel="stylesheet" href="./css/share.css">
+</head>
+<body>
+
+
+<script type="text/javascript" src="./js/share.js"></script>
+</body>
+</html>
