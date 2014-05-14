68877304ecd031d725e0e8659074bd2b8a95cdce
Yohei Munesada <y.munesada@gmail.com>
2014-05-14 23:14:30 +0900

    タップアクションを追加

--- js/src/views/pages/page_01/index.js
+++ js/src/views/pages/page_01/index.js
@@ -1,1 +1,30 @@
 // page01
+(function () {
+
+    // コンテナ
+    var $content = $('[data-page="1"]');
+
+    // Boxタップ
+    // 仮です（tapにします）
+    $content.on('click', '.jsBoxTap', function () {
+        console.debug('[jsBoxTap]');
+
+        var 
+            $box = $content.find('.jsBox'),
+            $this = $(this);
+
+        $box.addClass('rotateAnim');
+        $this.addClass('noAction');
+        setTimeout(function () {
+            $box.removeClass('rotateAnim');
+            $this.removeClass('noAction');
+        }, 800);
+
+    });
+
+
+
+
+
+
+})();
