caf3f3026a56f0cda4db265003267c47a04da50b
Yohei Munesada <y.munesada@gmail.com>
2014-05-25 23:23:46 +0900

    ロゴ部分のアニメーション追加

--- js/src/views/pages/page_00/index.js
+++ js/src/views/pages/page_00/index.js
@@ -1,1 +1,26 @@
 // page00
+;(function () {
+
+    $page = $('[data-page="0"]');
+
+
+
+    // ロゴ.
+    $page.on('click', '.jsLogo', function () {
+
+        $(this)
+            .addClass('anim noAction')
+            .wait(3000)
+            .removeClass('anim noAction');
+    });
+
+
+
+
+
+
+
+
+
+
+})();
