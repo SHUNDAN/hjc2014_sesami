21aa1cdce5a1e08eb7fbca542bf03c15a044e2fc
Yohei Munesada <y.munesada@gmail.com>
2014-05-26 13:17:05 +0900

    page00のタイトルアニメーションを作成

--- js/src/views/pages/page_00/index.js
+++ js/src/views/pages/page_00/index.js
@@ -1,18 +1,42 @@
 // page00
 ;(function () {
 
-    $page = $('[data-page="0"]');
+    var
+        $page = $('[data-page="0"]'),
+        TOUCH_START = sesami.event.TOUCH_START,
+        TOUCH_MOVE = sesami.event.TOUCH_MOVE,
+        TOUCH_END = sesami.event.TOUCH_END
+        ;
 
 
 
     // ロゴ.
-    $page.on('click', '.jsLogo', function () {
+    $page.on(TOUCH_END, '.tapLogo', function () {
 
-        $(this)
+        $page
+            .find('.jsLogo')
             .removeClass('anim')
             .addClass('anim')
-            .wait(1800)
+            .wait(2000)
             .removeClass('anim');
+    });
+
+
+    // タイトル
+    $page.on(TOUCH_START + ' mouseenter', '.tapWord', function () {
+        var pos = $(this).data('pos');
+        // console.debug('[tapWord]', pos);
+
+        $page
+            .find('.wordWrapper[data-pos="'+pos+'"]')
+            .addClass('scale')
+            .wait(800)
+            .removeClass('scale');
+
+        $(this)
+            .addClass('noAction')
+            .wait(800)
+            .removeClass('noAction');
     });
 
 
