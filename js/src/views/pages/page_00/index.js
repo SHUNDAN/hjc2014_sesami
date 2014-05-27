237c1dd84e31d5ca0be1384e0424822d476c0c97
Yohei Munesada <y.munesada@gmail.com>
2014-05-27 13:28:18 +0900

    page00のオスカーのアニメを制作

--- js/src/views/pages/page_00/index.js
+++ js/src/views/pages/page_00/index.js
@@ -39,7 +39,15 @@
             .removeClass('noAction');
     });
 
+    // オスカー
+    $page.on(TOUCH_END, '.tapOscar', function () {
 
+        $page
+            .find('.jsOscarAnim')
+            .addClass('anim noAction')
+            .wait(5000)
+            .removeClass('anim noAction');
+    });
 
 
 
