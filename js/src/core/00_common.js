64d63040935fe8649604902003bb176e3d1c40df
Yohei Munesada <y.munesada@gmail.com>
2014-06-03 09:27:24 +0900

    スマホ対応、iOS7.1向けのminimal-ui対応、minimal-uiのバグ対策、タップハイライトカラー禁止

--- js/src/core/00_common.js
+++ js/src/core/00_common.js
@@ -18,12 +18,26 @@ sesami.event.TOUCH_MOVE = (hasTapEvent ? 'touchmove' : 'mousemove');
 sesami.event.TOUCH_END = (hasTapEvent ? 'touchend' : 'mouseup');
 
 
+// UserAgent
+var userAgent = navigator.userAgent.toLowerCase();
+console.debug('[userAgent]', userAgent);
+sesami.isIphone = userAgent.indexOf('iphone') + 1;
+sesami.isIphone7_1 = sesami.isIphone && userAgent.indexOf('os 7_1') !== -1;
+sesami.isAndroid = userAgent.indexOf('android') + 1;
 
 
 
+// iPhoneとAndroidではスクロールを禁止する.
+if (sesami.isIphone || sesami.isAndroid) {
+    document.body.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    })
+}
 
-
-
+// iOS7.1用にminimal-uiを付けているので、iOS7.1の場合はそれのバグ回避のため、高さを140%くらいにする。
+if (sesami.isIphone7_1) {
+    $('#container').addClass('iphone7_1');
+}
 
 
 })();
