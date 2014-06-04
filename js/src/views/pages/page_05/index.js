4f5386bb20c74b3d31ff99959cb3485f6fc408b0
Yohei Munesada <y.munesada@gmail.com>
2014-06-04 20:19:45 +0900

    page05のアニメーション作成

--- js/src/views/pages/page_05/index.js
+++ js/src/views/pages/page_05/index.js
@@ -3,6 +3,79 @@ sesami.page05 = {};
 sesami.page05.init = function () {
     console.debug('page05 init is called.');
 
+
+    var
+        $page = $('[data-page="5"]'),
+        TOUCH_START = sesami.event.TOUCH_START,
+        TOUCH_MOVE = sesami.event.TOUCH_MOVE,
+        TOUCH_END = sesami.event.TOUCH_END
+        ;
+
+
+    var selectType = 1;
+
+    // 型選択
+    var changeKata = function (targetType) {
+        $page
+            .find('.kataArea')
+            .removeClass('select');
+
+        $page
+            .find('.kataArea.type' + selectType)
+            .addClass('select');        
+    }
+    $page.on(TOUCH_END, '.tapAreaKata', function () {
+        selectType = $(this).data('type');
+        changeKata(selectType);
+    });
+
+
+    // 型抜き
+    var addRandomStyle = function (element) {
+
+        // var signX = (Math.random() < .4 ? 1 : -1);
+        // var signY = (Math.random() < .4 ? 1 : -1);
+        var deltaX = Math.random() * 150 - 100;
+        var deltaY = Math.random() * 200 - 150;
+        var scale  = Math.max(10, Math.random() * 4 * 10);
+        var transform = 'translate('+deltaX+'%, '+deltaY+'%)';
+
+        $(element)
+            .css({
+                '-webkit-transform': transform,
+                '-moz-transform': transform,
+                '-ms-transform': transform,
+                'transform': transform,
+                'width': scale + '%'
+            });
+    };
+    $page.on(TOUCH_END, '.tapAreaBase', function () {
+        console.debug('[tapAreaBase]');
+
+        var image = new Image();
+        image.src = './img/page05/cookie1-' + selectType + '.png';
+        image.className = 'cookie';
+        image.onload = function () {
+            $page.find('.cookieArea').append(this);
+            var self = this;
+            setTimeout(function () {
+                addRandomStyle(self);
+            }, 1);
+
+            selectType++;
+            if (selectType > 3) {
+                selectType = 1;
+            }
+            changeKata(selectType);
+        };
+
+
+    });
+
+
+
+
+
 };
 
 
